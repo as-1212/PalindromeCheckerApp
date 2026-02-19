f764b8021ad2105dcefcdd31fea3afd124b1cbbe
as-1212 <adrizasrivastava06@gmail.com>
2026-02-19 09:50:14 +0530

    Delete src/UseCase1PalindromeCheckerApp.java

--- src/UseCase1PalindromeCheckerApp.java
+++
@@ -1,6 +0,0 @@
-//Use Case 1 : Application Entry & Welcome Message
-public class UseCase1PalindromeCheckerApp {
-    public static void main (String [] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-    }
-}
